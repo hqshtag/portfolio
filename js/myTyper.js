eb2a95030976ee9824868744fb2635c0dd346dc7
hqshtag <46681148+hqshtag@users.noreply.github.com>
2022-07-13 12:26:15 +0000

    Deploying to gh-pages from @ hqshtag/portfolio@0017b61370c00d321973aa50fbb81a97a20129bb 🚀

--- js/myTyper.js
+++ js/myTyper.js
@@ -12,7 +12,25 @@ keystrokeSound.addEventListener("canplaythrough", (event) => {
   /* the audio is now playable; play it if permissions allow */
 
   document.dispatchEvent(soundLoaded);
+  $('.notice>p').css('opacity', '1');
+  $(".loading-screen").css('desplay', 'none');
+
 });
+
+keystrokeSound.addEventListener('canplay', (event)=> {
+  $(".loading-screen").delay(250).queue(function(){
+  //  $(this).css('display', 'none');
+  $(this).css('opacity','0');
+
+    $(this).dequeue();
+
+    $('.notice>h3').css('opacity', '1');
+
+
+});
+
+
+})
 
 function playSound() {
   if (typeof keystrokeSound.loop == "boolean") {
@@ -32,9 +50,9 @@ function setSoundPos(pos) {
 
 const introOps = {
   strings: [
-    `Hey^80,^120 My Name is Mohamed Wajih Tagourty. 🖐`,
-    `You can address me as <strong>Mohamed</strong> or <strong>Wajih</strong>. ^600\n You can also call me <b>hqshtag</b>^70,^130 That's my alias.`,
-    `I'm looking forward to getting to know you and I appreciate your interest,^300 Thanks^50!`,
+    `Hey^80,^120 I'm a <span class="highlight"> Software Developer</span>. 🖐`,
+    `You can address me as <span class="highlight">Mohamed</span> or <span class="highlight">Wajih</span>. ^600\n You can also call me <span class="highlight">hqshtag</span>^70,^130 That's my alias.`,
+    `I'm <span class="highlight">looking forward </span> to getting to know you and  I appreciate your <span class="highlight">interest</span>,^300 Thanks^50!`,
   ],
   typeSpeed: 30,
   backSpeed: 4,
@@ -72,6 +90,13 @@ $(() => {
   console.log("Let's get in touche, and develop something together 🤝 ");
   console.log("I'll really appreciate your feedback");
   $(document).on("SOUND_LOADED", () => {
+
+    $("body").keypress(function(e) {
+      $(".fulldisplaybutton").fadeOut();
+      $("#playBtn").hide();
+      new Typed(".welcome-text", introOps);
+    });
+    
     $(".fulldisplaybutton").on("click", () => {
       //keystrokeSound.play();
       $(".fulldisplaybutton").fadeOut();
@@ -85,9 +110,36 @@ $(() => {
   });
 
   $(document).on("TYPING_COMPLETE", () => {
-    $("section.contact").addClass("showup");
-    $("h1.welcome-text").css('margin-top', "0");
+
+    procedeureDanimation();
+
 
     console.timeEnd("TYPING DURATION");
   });
 });
+
+function procedeureDanimation(){
+  //show up contact links
+  $("section.contact").addClass("showup");
+
+
+  //realign  & fadeout welcome text
+  setTimeout(function() {
+    $("h1.welcome-text").css('margin-top', "0");
+    $("h1.welcome-text").css("opacity", "0");
+   }, 2000);
+  //display the name title header
+  setTimeout(function() {
+    $("div#IdHeader").css("top", "3rem");
+      
+    $("div#IdHeader>#name").css("opacity", "1");
+    $("div#IdHeader>#title").css("opacity", "1");
+    $("div#IdHeader>#description").css("opacity", "1");
+
+  }, 3000);
+
+
+
+
+}
+
